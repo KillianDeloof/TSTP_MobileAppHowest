9535e13c83c8ce5bc32fd3987a4672125840b968
Deloof Killian <killiandeloof@gmail.com>
2017-06-06 09:43:26 +0200

    cameraservice and fileservice

--- MobileAppHowest/MobileAppHowest/Repositories/MediaPicker.cs
+++ MobileAppHowest/MobileAppHowest/Repositories/MediaPicker.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Media;
+using System.IO;
+using System.Linq;
 
 namespace MobileAppHowest.Repositories
 {
-    class MediaPicker
+    class MediaPicker : ICameraService
     {
-        
 
+        public async Task<byte[]> TakePhotosAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            var picture = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions());
+
+            if (picture == null)
+            {
+                return null;
+            }
+
+            var ms = new MemoryStream();
+
+            using (var s = picture.GetStream())
+            {
+                await s.CopyToAsync(destination: ms);
+            }
+
+            //ms.Seek(offset: 0, loc: SeekOrigin.Begin);
+
+            return ms.ToArray();
+        }
 
 
 
