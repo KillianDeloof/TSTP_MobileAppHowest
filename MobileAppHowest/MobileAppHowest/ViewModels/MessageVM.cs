22576cf617f74daea5b03ce6e79e122258341a90
Jorden Ysewyn <jorden.ysewyn@student.howest.be>
2017-06-06 16:23:55 +0200

    Aanmaak messagePage

---
+++ MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MobileAppHowest.ViewModels
+{
+    public class MessageVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+    }
+}
