1cfa79b78c486b5c5b6d4e3bb609145073863b99
Deloof Killian <killiandeloof@gmail.com>
2017-06-07 10:50:34 +0200

    Message viewmodel

--- MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
+++ MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+
 
 namespace MobileAppHowest.ViewModels
 {
     public class MessageVM : INotifyPropertyChanged
     {
+        Ticket t = new Ticket();
+
+
         public MessageVM()
         {
             SendCommand = new Command(SendClicked);
@@ -27,17 +32,38 @@ namespace MobileAppHowest.ViewModels
 
         private void SendClicked(object obj)
         {
-            throw new NotImplementedException();
+            t.Name = "name: killian.deloof";
+            t.Subject = "NEED KOFFIE";
+            t.PriorityId = new int?();
+            t.TopicId = new int?();
+            t.Email = "killian.deloof@student.howest.be";
+            t.Message = "De koffie automaat werkt niet!, i need my koffie";
+
+            //t.Forum = "campus participatie OHK";
+            t.Category = "automaten";
+
+
+            String res = await AzureMobileClient.DefaultClient.InvokeApiAsync<Ticket, string>("/api/OSTicket", t, System.Net.Http.HttpMethod.Post, null, System.Threading.CancellationToken.None);
         }
 
         private void AttachClicked(object obj)
         {
-            throw new NotImplementedException();
+            MediaFile photo = await MediaPicker.PickPhoto();
+            Attachment at = new Attachment();
+            at.Name = "photo";
+            at.Content = photo;
+            at.Type = "jpg";
+            t.Attachments.Add(at);
         }
 
         private void PictureClicked(object obj)
         {
-            throw new NotImplementedException();
+            MediaFile photo = await MediaPicker.TakePhoto();
+            Attachment at = new Attachment();
+            at.Name = "photo";
+            at.Content = photo;
+            at.Type = "jpg";
+            t.Attachments.Add(at);
         }
 
         private void LocationClicked(object obj)
