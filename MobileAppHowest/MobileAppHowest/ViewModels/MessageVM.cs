80c2dc1081ed0645c01779092bb35aa1d3ef2bdd
Windels Thijs <thijs.windels@live.be>
2017-06-07 10:22:26 +0200

    Databinding MessagePage

--- MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
+++ MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
@@ -2,14 +2,52 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MobileAppHowest.ViewModels
 {
     public class MessageVM : INotifyPropertyChanged
     {
+        public MessageVM()
+        {
+            SendCommand = new Command(SendClicked);
+            AttachCommand = new Command(AttachClicked);
+            PictureCommand = new Command(PictureClicked);
+            CategoryCommand = new Command(CategoryClicked);
+            LocationCommand = new Command(LocationClicked);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public Command SendCommand { get; }
+        public Command AttachCommand { get; }
+        public Command PictureCommand { get; }
+        public Command CategoryCommand { get; }
+        public Command LocationCommand { get; }
 
 
+        private void SendClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
 
+        private void AttachClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PictureClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LocationClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CategoryClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
