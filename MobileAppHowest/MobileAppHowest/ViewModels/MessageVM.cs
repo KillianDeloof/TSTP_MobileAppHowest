a198c77492815c34b2380bcc0f4a7ff59da6fe02
Windels Thijs <thijs.windels@live.be>
2017-06-07 11:29:09 +0200

    Minor MessageVM changes

--- MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
+++ MobileAppHowest/MobileAppHowest/ViewModels/MessageVM.cs
@@ -4,14 +4,16 @@ using System.ComponentModel;
 using System.Text;
 using Xamarin.Forms;
 using Plugin.Media.Abstractions;
-
+using MobileAppHowest.Models;
+using MobileAppHowest.Repositories;
+using System.Threading.Tasks;
 
 namespace MobileAppHowest.ViewModels
 {
     public class MessageVM : INotifyPropertyChanged
     {
-        //Ticket t = new Ticket();
-        //Room r = new Room();
+        Ticket t = new Ticket();
+        Room r = new Room();
 
         public MessageVM()
         {
@@ -32,15 +34,15 @@ namespace MobileAppHowest.ViewModels
 
         private void SendClicked(object obj)
         {
-            //t.Name = "name: killian.deloof";
-            //t.Subject = "NEED KOFFIE";
-            //t.PriorityId = new int?();
-            //t.TopicId = new int?();
-            //t.Email = "killian.deloof@student.howest.be";
-            //t.Message = "De koffie automaat werkt niet!, i need my koffie" + r;
+            t.Name = "name: killian.deloof";
+            t.Subject = "need koffie";
+            t.PriorityId = new int?();
+            t.TopicId = new int?();
+            t.Email = "killian.deloof@student.howest.be";
+            t.Message = "de koffie automaat werkt niet!, i need my koffie" + r;
 
-            ////t.Forum = "what is this field?";
-            //t.Category = "automaten";
+            //t.forum = "what is this field?";
+            t.Category = "automaten";
 
 
             //String res = await AzureMobileClient.DefaultClient.InvokeApiAsync<Ticket, string>("/api/OSTicket", t, System.Net.Http.HttpMethod.Post, null, System.Threading.CancellationToken.None);
@@ -48,22 +50,27 @@ namespace MobileAppHowest.ViewModels
 
         private void AttachClicked(object obj)
         {
-            //MediaFile photo = await MediaPicker.PickPhoto();
-            //Attachment at = new Attachment();
-            //at.Name = "photo";
-            //at.Content = photo;
-            //at.Type = "jpg";
-            //t.Attachments.Add(at);
+            GetAttachment();
         }
 
-        private void PictureClicked(object obj)
+        private async Task GetAttachment()
         {
-            //MediaFile photo = await MediaPicker.TakePhoto();
-            //Attachment at = new Attachment();
-            //at.Name = "photo";
+            MediaFile photo = await MediaPicker.PickPhoto();
+            Attachment at = new Attachment();
+            at.Name = "photo";
             //at.Content = photo;
-            //at.Type = "jpg";
-            //t.Attachments.Add(at);
+            at.Type = "jpg";
+            t.Attachments.Add(at);
+        }
+
+        private async Task PictureClicked(object obj)
+        {
+            MediaFile photo = await MediaPicker.TakePhoto();
+            Attachment at = new Attachment();
+            at.Name = "photo";
+            //at.Content = photo;
+            at.Type = "jpg";
+            t.Attachments.Add(at);
         }
 
         private void LocationClicked(object obj)
