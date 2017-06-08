16607fb74067fc639727df09906c596ef974654e
Windels Thijs <thijs.windels@live.be>
2017-06-08 15:31:08 +0200

    Aanmaken FloorPage

---
+++ MobileAppHowest/MobileAppHowest/Views/FloorPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MobileAppHowest.Models;
+using MobileAppHowest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppHowest.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FloorPage : ContentPage
+	{
+		public FloorPage(Ticket ticket)
+		{
+			InitializeComponent();
+            BindingContext = new FloorVM(Navigation, ticket);
+		}
+	}
+}
