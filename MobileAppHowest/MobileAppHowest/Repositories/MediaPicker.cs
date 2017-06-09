6e674e66585d4f555a712fccc3e09c738b38d7e8
Deloof Killian <killiandeloof@gmail.com>
2017-06-09 16:12:05 +0200

    Fixed jorden's fuckup

--- MobileAppHowest/MobileAppHowest/Repositories/MediaPicker.cs
+++ MobileAppHowest/MobileAppHowest/Repositories/MediaPicker.cs
@@ -29,7 +29,8 @@ namespace MobileAppHowest.Repositories
                 var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
                 {
                     Directory = "Receipts",
-                    Name = $"{DateTime.UtcNow}.jpg"
+                    Name = $"{DateTime.UtcNow}.jpg",
+                    DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Rear
                 };
 
                 // Take a photo of the business receipt.
@@ -78,57 +79,7 @@ namespace MobileAppHowest.Repositories
         }
 
 
-        //chomado style
-        //public async Task<byte[]> TakePhotosAsync()
-        //{
-        //    await CrossMedia.Current.Initialize();
-
-        //    var picture = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions());
-
-        //    if (picture == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    var ms = new MemoryStream();
-
-        //    using (var s = picture.GetStream())
-        //    {
-        //        await s.CopyToAsync(destination: ms);
-        //    }
-
-        //    //ms.Seek(offset: 0, loc: SeekOrigin.Begin);
-
-        //    return ms.ToArray();
-        //}
-
-        //use this methot to call camera from viewmodel
-        //private async Task StartGameAsync()
-        //{
-        //    this.IsBusy = true;   (isbussy is a prop declared in the viewmodel)
-        //    try
-        //    {
-        //        this.Picture = await this.CameraService.TakePhotosAsync();
-        //        if (this.Picture == null)
-        //        {
-        //            await this.NavigationService.GoBackAsync();
-        //            return;
-        //        }
-
-        //        var detectResults = await this.DetectPictureAsync();
-
-        //        if (detectResults.Any())
-        //        {
-        //            this.DetectWinner(detectResults);
-        //        }
-
-        //        this.FaceDetectionResults = detectResults;
-        //    }
-        //    finally
-        //    {
-        //        this.IsBusy = false;
-        //    }
-        //}
+       
 
 
     }
