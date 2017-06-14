0cabefce32a6cb7a3b2314a827f106fdb129fa85
Deloof Killian <killiandeloof@gmail.com>
2017-06-14 11:18:11 +0200

    packages

---
+++ MobileAppHowest/MobileAppHowest/Repositories/ISQLite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using SQLite.Net;
+
+namespace MobileAppHowest.Repositories
+{
+
+    public interface ISQLite
+    {
+        //SQLiteConnection GetConnection();
+    }
+
+}
