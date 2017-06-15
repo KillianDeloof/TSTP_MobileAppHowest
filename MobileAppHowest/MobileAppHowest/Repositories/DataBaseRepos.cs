b5d963a5fc92c9c3e85fbab5766f2880a07641ae
Deloof Killian <killiandeloof@gmail.com>
2017-06-15 14:59:11 +0200

    DatabaseRepos

--- MobileAppHowest/MobileAppHowest/Repositories/DataBaseRepos.cs
+++ MobileAppHowest/MobileAppHowest/Repositories/DataBaseRepos.cs
@@ -7,24 +7,141 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Linq;
 
+/* ------------------------------------------ how to use ---------------------------
+ public MainPageViewModel() {
+  ...
+  var database = new Database("People"); // Creates (if does not exist) a database named People
+  database.CreateTable<Person>(); // Creates (if does not exist) a table of type Person
+  ...
+}
 
+
+    Classes that needs to be saved to database need to inherit from  "MobileAppHowest.Models.BaseItem"    !!!!!!!!!!!!!!!!!
+     
+     */
 
 namespace MobileAppHowest.Repositories
 {
     public class DataBaseRepos// : ISQLite
     {
+
+        static object locker = new object();
+
+        ISQLite SQLite
+        {
+            get
+            {
+                return DependencyService.Get<ISQLite>();
+            }
+        }
+        readonly SQLiteConnection _connection;
         readonly string DatabaseName;
 
 
-        private SQLiteConnection _connection;
+
 
         public DataBaseRepos(string databaseName)
         {
             DatabaseName = databaseName;
             //connection = SQLite.GetConnection(DatabaseName);
             _connection = DependencyService.Get<ISQLite>().GetConnection(DatabaseName);//get a registered class that implements the ISQLite interface and call its GetConnection method.
-            _connection.CreateTable<UserInfo>();//create the table, if it doesn't already exist
         }
+
+
+        public long GetSize()
+        {
+            return SQLite.GetSize(DatabaseName);
+        }
+
+
+        /// <summary>
+        /// create a table in the databace, if it doesn't already exist
+        /// </summary>
+        /// <typeparam name="T">The ObjectType of the table</typeparam>
+        public void CreateTable<T>()
+        {
+            lock (locker)
+            {
+                _connection.CreateTable<T>();
+            }
+        }
+
+
+
+        public int SaveItem<T>(T item)
+        {
+            lock (locker)
+            {
+                var id = ((BaseItem)(object)item).ID;
+                if (id != 0)
+                {
+                    _connection.Update(item);
+                    return id;
+                }
+                else
+                {
+                    return _connection.Insert(item);
+                }
+            }
+        }
+
+        public void ExecuteQuery(string query, object[] args)
+        {
+            lock (locker)
+            {
+                _connection.Execute(query, args);
+            }
+        }
+
+        public List<T> Query<T>(string query, object[] args) where T : new()
+        {
+            lock (locker)
+            {
+                return _connection.Query<T>(query, args);
+            }
+        }
+
+        public IEnumerable<T> GetItems<T>() where T : new()
+        {
+            lock (locker)
+            {
+                return (from i in _connection.Table<T>() select i).ToList();
+            }
+        }
+
+        public int DeleteItem<T>(int id)
+        {
+            lock (locker)
+            {
+                return _connection.Delete<T>(id);
+            }
+        }
+
+        public int DeleteAll<T>()
+        {
+            lock (locker)
+            {
+                return _connection.DeleteAll<T>();
+            }
+        }
+
+
+
+
+
+
+        
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 
         public IEnumerable<UserInfo> GetUsers()
         {
@@ -42,16 +159,7 @@ namespace MobileAppHowest.Repositories
             _connection.Delete<UserInfo>(id);
         }
 
-        public void AddUser(string thought)
-        {
-            var newThought = new UserInfo
-            {
-                FirstName = thought,
-                LastName = "lastName"
-            };
 
-            _connection.Insert(newThought);
-        }
 
 
         //---------------------------------------------------
