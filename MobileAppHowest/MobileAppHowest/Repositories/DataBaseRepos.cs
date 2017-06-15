65177723607b855856e4852e0c63ce44c2a49a10
Deloof Killian <killiandeloof@gmail.com>
2017-06-15 15:07:20 +0200

    Database repos

--- MobileAppHowest/MobileAppHowest/Repositories/DataBaseRepos.cs
+++ MobileAppHowest/MobileAppHowest/Repositories/DataBaseRepos.cs
@@ -141,125 +141,12 @@ namespace MobileAppHowest.Repositories
         //------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-
-        public IEnumerable<UserInfo> GetUsers()
-        {
-            return (from t in _connection.Table<UserInfo>()
-                    select t).ToList();
-        }
-
         public UserInfo GetUser(int id)
         {
             return _connection.Table<UserInfo>().FirstOrDefault(t => t.ID == id);
         }
 
-        public void DeleteUser(int id)
-        {
-            _connection.Delete<UserInfo>(id);
-        }
 
-
-
-
-        //---------------------------------------------------
-
-
-        //private async Task<string> insertUpdateData(UserInfo data, string path)
-        //{
-        //    try
-        //    {
-        //        var db = new SQLiteAsyncConnection(path);
-        //        if (await db.InsertAsync(data) != 0)
-        //            await db.UpdateAsync(data);
-        //        return "Single data file inserted or updated";
-        //    }
-        //    catch (SQLiteException ex)
-        //    {
-        //        return ex.Message;
-        //    }
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
-        //-----------------------------------------data------------------------------------------
-        public UserInfo GetUserInfo()
-        {
-            return null;
-        }
-        public List<Campus> GetCampusList()
-        {
-            return null;
-        }
-        public List<Building> GetBuildingList()
-        {
-            return null;
-        }
-        public List<Building> GetBuildingList(Campus campusFilter)
-        {
-            return null;
-        }
-        public List<Floor> GetFloorList()
-        {
-            return null;
-        }
-        public List<Floor> GetFloorList(Building buildingFilter)
-        {
-            return null;
-        }
-        public List<Room> GetRoomList()
-        {
-            return null;
-        }
-        public List<Room> GetRoomList(Floor floorFilter)
-        {
-            return null;
-        }
-        public List<MainCategory> GetMainCategorys()
-        {
-            return null;
-        }
-        public List<Category> GetCategorys(MainCategory mainCat)
-        {
-            return null;
-        }
-        public void SafeUserInfo(UserInfo user)
-        {
-
-        }
-        public void SafeCampusList(List<Campus> campusList)
-        {
-
-        }
-        public void SafeBuildingList(List<Building> buildingList)
-        {
-
-        }
-        public void SafeFloorList(List<Floor> floorList)
-        {
-
-        }
-        public void SafeRoomList(List<Room> roomList)
-        {
-
-        }
-        public void SafeMainCatList(List<MainCategory> mainCatList)
-        {
-
-        }
-        public void SafeCatList(List<Category> catList)
-        {
-
-        }
 
 
     }
