92cda26a017252465d4ab03a43bb4216aa0178e1
Windels Thijs <thijs.windels@live.be>
2017-06-22 13:01:46 +0200

    Laatste kleine wijzigingen

--- MobileAppHowest.android.test/Resources/Resource.Designer.cs
+++ MobileAppHowest.android.test/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace MobileAppHowest.android.test
 		
 		public static void UpdateIdValues()
 		{
-			global::Splat.Resource.String.library_name = global::MobileAppHowest.android.test.Resource.String.library_name;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionHostName = global::MobileAppHowest.android.test.Resource.Id.OptionHostName;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionPort = global::MobileAppHowest.android.test.Resource.Id.OptionPort;
 			global::Xamarin.Android.NUnitLite.Resource.Id.OptionRemoteServer = global::MobileAppHowest.android.test.Resource.Id.OptionRemoteServer;
@@ -175,14 +174,11 @@ namespace MobileAppHowest.android.test
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040002
-			public const int ApplicationName = 2130968578;
-			
 			// aapt resource value: 0x7f040001
-			public const int Hello = 2130968577;
+			public const int ApplicationName = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int library_name = 2130968576;
+			public const int Hello = 2130968576;
 			
 			static String()
 			{
